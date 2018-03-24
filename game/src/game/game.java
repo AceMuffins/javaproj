35f189131e9b661b30dce59e325630fce16efa13
Zenliss <36843738+Zenliss@users.noreply.github.com>
2018-03-24 12:33:46 +0200

    Add files via upload

---
+++ game/src/game/game.java
@@ -0,0 +1,17 @@
+package game;
+	
+import javax.swing.*;
+
+
+public class game {
+
+	public static void main(String[] args) {
+		String res = JOptionPane.showInputDialog(null, "¬ведите сложность игры от 1 до 7:", "—ложность игры", 1);
+		int slogn = res.charAt(0)-'0';
+		if((slogn >= 1) && (slogn <= 7)) {
+			okno window = new okno(slogn);
+		}
+			
+	}
+	
+}
